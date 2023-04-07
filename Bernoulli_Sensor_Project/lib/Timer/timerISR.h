a477ac93bf9826664dcceb395db63dc7fb7444ab
Nils_PC <nils.bestehorn@gmail.com>
2023-04-07 18:50:26 +0200

    Testing seperating Timer

---
+++ Bernoulli_Sensor_Project/lib/Timer/timerISR.h
@@ -0,0 +1,12 @@
+#ifndef timerISR_H  
+#define timerISR_H 
+
+//Declarations...
+#define ledPin  13
+int timer1_compare_match;
+
+void setTimerISR();
+
+
+
+#endif
--- Bernoulli_Sensor_Project/src/main.cpp
+++ Bernoulli_Sensor_Project/src/main.cpp
@@ -1,7 +1,30 @@
 #include <Arduino.h>
+#include "timerISR.h"
+
+// Define LED Pin
+#define ledPin  13
+
+// Define timer compare match value
+int timer1_compare_match;
+
+// Interrupt Service Routine for compare mode
+ISR(TIMER1_COMPA_vect){
+  // Preload timer with compare match value
+  TCNT1 = timer1_compare_match;
+
+  // Write opposite value to LED
+  digitalWrite(ledPin, digitalRead(ledPin)^1);
+}
+
 
 void setup() {
   // put your setup code here, to run once:
+
+  // Set LED as outupt
+  pinMode(ledPin, OUTPUT);	
+  
+
+ void setTimerISR();
 }
 
 void loop() {
