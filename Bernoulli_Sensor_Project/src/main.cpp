4c964a83ca07e48c9a9eab4ba622fbea2acefe88
Nils_PC <nils.bestehorn@gmail.com>
2023-04-12 00:34:36 +0200

    Added Test Serial Out's for better Messerment of the output Data; Next is a Test of the ouput Data

--- Bernoulli_Sensor_Project/src/main.cpp
+++ Bernoulli_Sensor_Project/src/main.cpp
@@ -1,23 +1,37 @@
 //Include Libraries
 #include <Arduino.h>
-
-//Include Own Code-Snippets
-#include "timerISR.h"
+#include <FlowSensor.h>
 
 
+// pin -> interrupt pin
+FlowSensor Sensor(YFS201, 2); //2=>D2
+unsigned long timebefore = 0; // Same type as millis()
+
+
+void count(){
+  Sensor.count();
+}
 
 
 
 void setup() {
-  // put your setup code here, to run once:
-
-  // Set LED as outupt
-  pinMode(ledPin, OUTPUT);	
-  
-
- void setTimerISR();
+  Serial.begin(115200);
+  Sensor.begin(count);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+    if (millis() - timebefore >= 1000) //1000 = 1sec
+  {
+    Sensor.read();// calibrate = Number in read() [_numpuls+calibrate]
+    Serial.print("Flow rate (L/min) : ");
+    Serial.print(Sensor.getFlowRate_m());
+    Serial.print("\tTotal Pulse count: ");
+    Serial.print(Sensor.getPulse());
+    Serial.print("\tMillis: ");
+    Serial.print(millis());
+    Serial.print("\tTime Before: ");
+    Serial.println(timebefore);
+    timebefore = millis();
+  }
 }
