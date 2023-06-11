6750a25248e2edadaa73c62707a20d2d9b435120
Nils Bestehorn <104395997+nilbest@users.noreply.github.com>
2023-06-11 17:00:28 +0200

    Test LCD Screen Liquid Crisatl

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,23 @@
 //Include Libraries
 #include <Arduino.h>
-#include <FlowSensor.h>
+#include <LiquidCrystal.h>
 
 
-// pin -> interrupt pin
-FlowSensor Sensor(YFS201, 2); //2=>D2
-unsigned long timebefore = 0; // Same type as millis()
-
-
-void count(){
-  Sensor.count();
-}
-
-
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-  Serial.begin(115200);
-  Sensor.begin(count);
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    if (millis() - timebefore >= 1000) //1000 = 1sec
-  {
-    Sensor.read();// calibrate = Number in read() [_numpuls+calibrate]
-    Serial.print("Flow (L/min) : ");
-    Serial.print(Sensor.getFlowRate_m());
-    Serial.print("\tAll Puls count: ");
-    Serial.print(Sensor.getPulse());
-    /*Serial.print("\t\tMillis: ");
-    Serial.print(millis());
-    Serial.print("\tTime Before: ");
-    Serial.print(timebefore);*/
-    Serial.print("\tFl sec: ");
-    Serial.print(Sensor.getFlowRate_s());
-    Serial.print("\tLitter: ");
-    Serial.println(Sensor.getVolume());
-    timebefore = millis();
-  }
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
 }
