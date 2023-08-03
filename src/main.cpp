aa60cb24e8499a0469e6349dacd76e25a608a2c4
Nils Bestehorn <104395997+nilbest@users.noreply.github.com>
2023-08-03 19:02:20 +0200

    Test Basic Skatch

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,76 @@
+
 //Include Libraries
 #include <Arduino.h>
 
-//#include"PressureSensor.h"
+// Pins am Arduino für HX711
+const int DOUT_PIN = 2;
+const int SCK_PIN = 3;
 
-#include "HX711.h"
-
-const int LOADCELL_DOUT_PIN = 2;
-const int LOADCELL_SCK_PIN = 3;
-
-HX711 scale;
+// Kalibrierungsfaktoren für deinen Drucksensor (müssen entsprechend deinem Sensor angepasst werden)
+const float SCALE_FACTOR = 1.0;  // Skalierungsfaktor zur Umrechnung in Druckwerte
+const float OFFSET = 0.0;        // Offset-Korrektur (falls nötig)
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("Serial initialization");
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN, 128);
+  pinMode(DOUT_PIN, INPUT);
+  pinMode(SCK_PIN, OUTPUT);
+}
+
+long readHX711() {
+  long value = 0;
+  byte data[3] = {0};
+  
+  while (digitalRead(DOUT_PIN));  // Warte, bis DOUT auf LOW geht
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 7; j >= 0; --j) {
+      digitalWrite(SCK_PIN, HIGH);
+      bitWrite(data[i], j, digitalRead(DOUT_PIN));
+      digitalWrite(SCK_PIN, LOW);
+    }
+  }
+  
+  digitalWrite(SCK_PIN, HIGH);
+  digitalWrite(SCK_PIN, LOW);
+  
+  // Setze den ADC-Wandler in den Leerzustand
+  data[2] ^= 0x80;
+  value = (static_cast<long>(data[2]) << 16) | (static_cast<long>(data[1]) << 8) | data[0];
+  
+  return value;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //Serial.println("Test");
-  Serial.println(scale.is_ready());
-  if (scale.is_ready()) {
-    long reading = scale.read();
-    Serial.print("HX711 reading: ");
-    Serial.print(reading);
-    Serial.print("\tAverage: ");
-    long average = (scale.read_average()); 
-    Serial.println(average);
-  } else {
-    Serial.println("HX711 not found.");
-  }
-
+  long rawValue = readHX711();
   
-  delay(1000);
+  // Hier musst du die spezifische Kalibrierung für deinen MPS2107-Sensor vornehmen,
+  // um die tatsächliche Druckmessung zu berechnen. Der genaue Zusammenhang zwischen
+  // dem Rohwert (rawValue) und dem Druck hängt von den spezifischen Eigenschaften deines Sensors ab.
+
+  // Beispiel: Annahme, dass der MPS2107 0.5 V bei 0 PSI (0 kPa) und 4.5 V bei 100 PSI (689.48 kPa) ausgibt.
+  // Beachte, dass dies nur ein Beispiel ist und du deine eigene Kalibrierung durchführen musst.
+  float voltage = (rawValue * 5.0) / static_cast<float>(0x7FFFFF);
+  float pressure_psi = (voltage - 0.5) * (100.0 / 4.0);
+  float pressure_kpa = pressure_psi * 6.89476;
+
+  // Anwenden der Kalibrierungsfaktoren
+  pressure_psi = (pressure_psi * SCALE_FACTOR) + OFFSET;
+  pressure_kpa = (pressure_kpa * SCALE_FACTOR) + OFFSET;
+
+  // Ausgabe auf der seriellen Konsole
+  Serial.print("Rohwert: ");
+  Serial.print(rawValue);
+  
+  Serial.print(", Spannung: ");
+  Serial.print(voltage, 4);
+  Serial.print(" V");
+
+  Serial.print(", Druck (PSI): ");
+  Serial.print(pressure_psi, 2);
+  Serial.print(" PSI");
+
+  Serial.print(", Druck (kPa): ");
+  Serial.print(pressure_kpa, 2);
+  Serial.println(" kPa");
+
+  delay(1000);  // Eine Sekunde warten, bevor die nächste Messung durchgeführt wird
 }
-
-
-
-
